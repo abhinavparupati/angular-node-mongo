50a279e196156c103a84863cae0d105ca2c3516f
Parupati Abhinav <abhinavparupati@gmail.com>
2025-07-27 14:31:49 +0530

    Revert "feat: Modify UI"

--- ang-frontend/src/app/app.component.ts
+++ ang-frontend/src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [RouterOutlet],
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">People Management</a>
+      </div>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
-  title = 'ang-frontend';
+  title = 'People Management';
 }
